9b304278987247791c11247b238a0b595d796af7
Your Name <linus.m.muema@gmail.com>
2019-04-15 01:10:56 +0300

    Edit the squad class

--- src/main/java/Squad.java
+++ src/main/java/Squad.java
@@ -1,4 +1,21 @@
 public class Squad {
+    private String mName;
+    private String mSize;
+    private  String mCause;
     public Squad(String name, String size, String cause) {
+        mName= name;
+        mSize= size;
+        mCause= cause;
+    }
+    public String getmName() {
+        return mName;
+    }
+
+    public String getmSize() {
+        return mSize;
+    }
+
+    public String getmCause() {
+        return mCause;
     }
 }
