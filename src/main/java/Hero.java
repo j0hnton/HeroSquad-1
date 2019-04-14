1b7896c6da9ec219261e4fa206be24240eed0f09
Your Name <linus.m.muema@gmail.com>
2019-04-15 01:07:48 +0300

    Edit the hero class

--- src/main/java/Hero.java
+++ src/main/java/Hero.java
@@ -1,4 +1,29 @@
 public class Hero {
+    private String mName;
+    private String mAge;
+    private String mSuperpower;
+    private String mWeakness;
+
     public Hero(String name, String age, String superpower, String weakness) {
+        mName = name;
+        mAge = age;
+        mSuperpower = superpower;
+        mWeakness = weakness;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public String getmAge() {
+        return mAge;
+    }
+
+    public String getmSuperpower() {
+        return mSuperpower;
+    }
+
+    public String getmWeakness() {
+        return mWeakness;
     }
 }
