29240f1b273de3858482cf3fa4477af5201b4fbe
Your Name <linus.m.muema@gmail.com>
2019-04-15 00:08:50 +0300

    Edit the css file

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,23 +1,45 @@
-import java.util.Map;
-import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import static spark.Spark.*;
 
-public class  App {
+public class App {
     public static void main(String[] args) {
-        String layout = "layout.vtl";
         staticFileLocation("/public");
+
+        ProcessBuilder process = new ProcessBuilder();
+        Integer port;
+        if (process.environment().get("PORT") != null) {
+            port = Integer.parseInt(process.environment().get("PORT"));
+        } else {
+            port = 4567;
+        }
+
+        port(port);
+
+        String layout = "public/templates/layout.vtl";
+
 
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            model.put("template", "index.vtl");
+            model.put("template", "public/templates/index.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
 
+
         get("/squad", (request, response) -> {
             Map<String, Object> model = new HashMap<String, Object>();
-            model.put("template", "squad.vtl");
+            model.put("template", "public/templates/squad.vtl");
+            return new ModelAndView(model, layout);
+        }, new VelocityTemplateEngine());
+
+        get("/display", (request, response) -> {
+            Map<String, Object> model = new HashMap<String, Object>();
+            model.put("template", "public/templates/display.vtl");
             return new ModelAndView(model, layout);
         }, new VelocityTemplateEngine());
     }
